692aa6d14d568652855672e8aab8fec4c13277fb
nazil34 <nzimkani@gmail.com>
2025-06-11 17:31:20 +0800

    starting deployment

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const init = async () => {
 
   const server = Hapi.server({
     port: process.env.PORT || 3000,
-    host: process.env.HOST || "localhost",
+    host: '0.0.0.0',
     routes: {
       cors: {
         origin: ["*"],
